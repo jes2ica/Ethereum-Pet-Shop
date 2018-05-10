5a6a1f176b97b457c38f709494d9f7cdbc3ad52c
Yijie <jes2icazhuang@gmail.com>
2018-05-10 13:58:30 -0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -29,10 +29,9 @@ App = {
       App.web3Provider = web3.currentProvider;
     } else {
       // If no injected web3 instance is detected, fall back to Ganache
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
     }
     web3 = new Web3(App.web3Provider);
-
     return App.initContract();
   },
 
@@ -41,14 +40,11 @@ App = {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       var AdoptionArtifact = data;
       App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-
       // Set the provider for our contract
       App.contracts.Adoption.setProvider(App.web3Provider);
-
       // Use our contract to retrieve and mark the adopted pets
       return App.markAdopted();
     });
-
     return App.bindEvents();
   },
 
@@ -58,10 +54,8 @@ App = {
 
   markAdopted: function(adopters, account) {
     var adoptionInstance;
-
     App.contracts.Adoption.deployed().then(function(instance) {
       adoptionInstance = instance;
-
       return adoptionInstance.getAdopters.call();
     }).then(function(adopters) {
       for (i = 0; i < adopters.length; i++) {
@@ -76,21 +70,16 @@ App = {
 
   handleAdopt: function(event) {
     event.preventDefault();
-
     var petId = parseInt($(event.target).data('id'));
-
     var adoptionInstance;
-
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
       }
-
       var account = accounts[0];
-
+      console.log('account:' + account);
       App.contracts.Adoption.deployed().then(function(instance) {
         adoptionInstance = instance;
-
         // Execute adopt as a transaction by sending account
         return adoptionInstance.adopt(petId, {from: account});
       }).then(function(result) {
@@ -100,7 +89,6 @@ App = {
       });
     });
   }
-
 };
 
 $(function() {
